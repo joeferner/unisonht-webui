29a07d8f3bc20f99c2650be28eeb75d0819b9997
Joe Ferner <joe@fernsroth.com>
2020-10-09 11:12:29 -0400

    add status and capital case

--- public/Device.jsx
+++ public/Device.jsx
@@ -1,12 +1,14 @@
 function Device(props) {
     const {deviceName, onButtonClick, onError} = props;
     const [buttons, setButtons] = React.useState(undefined);
+    const [status, setStatus] = React.useState(undefined);
 
     React.useEffect(() => {
         const loadDevice = async () => {
             try {
                 const response = await axios.get(`/device/${deviceName}`);
                 setButtons(response.data.buttons);
+                setStatus(response.data);
             } catch (err) {
                 onError(err);
             }
@@ -14,6 +16,14 @@ function Device(props) {
         loadDevice();
     }, []);
 
-    return (<Buttons buttons={buttons} onClick={(buttonName) => onButtonClick(deviceName, buttonName)}/>);
+    return (<div>
+        <Buttons buttons={buttons} onClick={(buttonName) => onButtonClick(deviceName, buttonName)}/>
+        <MaterialUI.Accordion>
+            <MaterialUI.AccordionSummary><MaterialUI.Typography>Status</MaterialUI.Typography></MaterialUI.AccordionSummary>
+            <MaterialUI.AccordionDetails>
+                <pre>{JSON.stringify(status, null, 2)}</pre>
+            </MaterialUI.AccordionDetails>
+        </MaterialUI.Accordion>
+    </div>);
 }
 
